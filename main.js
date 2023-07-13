19a850b6456fd5a06769cd2e07ecb74aaa711391
epictahir1 <123293606+epictahir1@users.noreply.github.com>
2023-07-13 14:53:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var first_name="Ranveer";
+var last_name="Kapoor";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var a=10;
+var b=5;
+var add=a+b;
+var sub=a-b;
+var mul=a*b;
+var div=a/b;
+
+console.log(add);
+console.log(sub);
+console.log(mul);
+console.log(div);
